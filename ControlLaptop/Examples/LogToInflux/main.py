af42b587877c685fe330b8d1d7d2012f073ece8d
Colton Smith <47944858+colton-smith@users.noreply.github.com>
2020-02-15 17:39:55 -0500

    Add python main to create and log to db

---
+++ ControlLaptop/Examples/LogToInflux/main.py
@@ -0,0 +1,60 @@
+# Example for logging data to influxdb using python
+# Sourced from the influxdb python docs
+
+import random
+import time
+from datetime import datetime 
+from influxdb import InfluxDBClient
+
+# Example data payload
+# Tags contain metadata about the measurement - always a string
+# Thinking we could have something like below for each measurement
+
+def main():
+        
+    telemetry = [
+        {
+            "measurement": "temperature",
+            "tags": {
+                "host": "server01",
+                "region": "us-west"
+            },
+            #"time": f"{datetime.now()}",
+            "fields": {
+                "Celsius": 1.0,
+                "Fahrenheit": 3,
+            }
+        }
+    ]
+
+    # Create client instance
+    # host, port, user, pass, database
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
+
+    # client.drop_database('example')
+    # Create a new database in influx
+    client.create_database('example')
+
+    while 1:
+
+        print(f"C: {telemetry[0]['fields']['Celsius']}")
+        print(f"F: {telemetry[0]['fields']['Fahrenheit']}")
+        print()
+
+        # Writing the payload to the DB
+        client.write_points(telemetry)
+
+        # Scramble data
+        telemetry[0]['fields']['Celsius'] = random.random() * 100.0 
+        telemetry[0]['fields']['Fahrenheit'] = random.randint(0,100) 
+    #s telemetry[0]['time'] = datetime.now()
+
+        time.sleep(1)
+
+    # Print the results of the query
+    result = client.query('select Celsius,Fahrenheit from temperature;')
+    print(f"Inserted: {result}")
+    print()
+
+if __name__ == "__main__":
+    main()
