036b1115c41b1761aa4c3a8d0a4a82d96f63042f
Daniel Burke <danielseanburke@gmail.com>
2020-02-18 17:59:26 -0330

    Add sample program to confirm PIO envs are working

--- Node/DTS/main.cpp
+++ Node/DTS/main.cpp
@@ -1,1 +1,14 @@
-// This file is intentionally left blank
+#include <Arduino.h>
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+}
