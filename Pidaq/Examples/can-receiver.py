9b60f4470121224cc517069e7e327376d60ab4a8
Daniel Burke <danielseanburke@gmail.com>
2020-03-19 13:36:53 -0230

    Create can-receiver script

---
+++ Pidaq/Examples/can-receiver.py
@@ -0,0 +1,47 @@
+import json
+import can
+
+# Function Definitions
+
+
+def create_can_filters(raw_filters):
+    """ Creates a list of can filter objects
+
+    Parameters:
+    raw_filters (list): list of message filter objects as defined in the json schema
+
+    Returns:
+
+    can_filters (list): list of python-can filter objects
+
+    """
+    can_filters = []
+    for raw_can_filter in raw_can_filters:
+        can_id = raw_can_filter["canId"]
+        can_mask = raw_can_filter["canMask"]
+        extended = raw_can_filter["extended"]
+        can_filters.append({
+            "can_id": can_id,
+            "can_mask": can_mask,
+            "extended": extended
+        })
+    return can_filters
+
+
+# Main Script
+
+if __name__ == "__main__":
+    json_config = open('receiverInfo.json', mode='r')
+    can_config = json.load(json_config)
+    can_bus = can.Bus(bustype=can_config["canInfo"]["busType"],
+                      channel=can_config["canInfo"]["serialPort"],
+                      bitrate=can_config["canInfo"]["bitrate"])
+    raw_can_filters = [
+        can_filter for can_filter in can_config["canInfo"]["busFilters"]]
+    can_filters = create_can_filters(raw_can_filters)
+    can_bus.set_filters(filters=can_filters)
+    for message in can_bus:
+        message_file = open('messages.txt', mode='a+')
+        print(message)
+        message_file.write(f'{message.data}\r\n')
+        message_file.close()
