701a6f6f83cabeab9e9a351910a26764741ef1f7
Daniel Burke <danielseanburke@gmail.com>
2020-03-29 14:31:54 -0230

    Begin adding constant hex values and methods

--- Node/Common/drivers/inc/adc_ADS114S0.h
+++ Node/Common/drivers/inc/adc_ADS114S0.h
@@ -16,6 +16,26 @@ public:
     uint16_t *getuAdcData() { return uAdcData; };
 
 private:
+    SPIClass spi;
+    SPISettings spiSettings = SPISettings(20000000, MSBFIRST, SPI_MODE0);
+    uint8_t SS_PIN;
+    uint8_t POWER_SEQ_PIN;
+    bool HAS_SEQ_PIN = false;
+    // 16-bit data words for SPI
+    const uint16_t SET_CHANNEL_REG = 0x8000;
+    const uint16_t CONFIG_PROGRAM_REG = 0x2840;
+    const uint16_t MANUAL_READ = 0x1000;
+    const uint16_t AUTO_READ_RESET = 0x2C40;
+    const uint16_t AUTO_READ_NEXT = 0x2000;
+    const uint8_t WAKEUP = 0x02;
+    const uint8_t POWERDOWN = 0x04;
+    const uint8_t RESET = 0x06;
+
+    uint16_t uAdcData[16]{};      //array of the latest data; 0 for channels not being used
+    uint16_t uActiveChannels = 0; // each bit represents a channel; 1=used, 0=unused
+    uint16_t uNumChannels = 0;    // number of channels being used;
+
+    uint16_t transfer(uint16_t uData);
 };
 
 #endif
--- Node/Common/drivers/src/adc_ADS114S0.cpp
+++ Node/Common/drivers/src/adc_ADS114S0.cpp
@@ -1,1 +1,17 @@
 #include "adc_ADS114S0.h"
+
+ADS114S0::ADS114S0(SPIClass spi, uint8_t SS_PIN, uint8_t POWER_SEQ_PIN) : spi(spi), SS_PIN(SS_PIN), POWER_SEQ_PIN(POWER_SEQ_PIN)
+{
+    HAS_SEQ_PIN = true;
+}
+
+ADS114S0::ADS114S0(SPIClass spi, uint8_t SS_PIN) : spi(spi), SS_PIN(SS_PIN)
+{
+}
+
+void ADS114S0::init()
+{
+    spi.begin();
+    pinMode(SS_PIN, OUTPUT);
+    digitalWrite(SS_PIN, HIGH);
+}
