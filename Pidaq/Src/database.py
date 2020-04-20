8672cf0a0baf0fe4fe49ee6fcc355ec6110f350f
Daniel Burke <danielseanburke@gmail.com>
2020-04-20 19:53:43 -0230

    create method for creating a retention policy

--- Pidaq/Src/database.py
+++ Pidaq/Src/database.py
@@ -3,12 +3,14 @@ import os
 
 from influxdb import InfluxDBClient
 
+
 class Influx:
     """
     """
 
-    def __init__(self, database): 
+    def __init__(self, database):
         self.client = InfluxDBClient(host='localhost', port=8086)
+        self.current_database = database
         current_databases = self.client.get_list_database()
         if not any(current_database['name'] == database for current_database in current_databases):
             self.client.create_database(database)
@@ -16,9 +18,9 @@ class Influx:
 
     def switch_database(self, database):
         self.client.switch_database(database)
-         
+
     def log_data(self, data, tags):
-        table_row =[{
+        table_row = [{
             'measurement': 'sensor_data',
             'time': datetime.datetime.now(),
             'fields': data,
@@ -27,4 +29,12 @@ class Influx:
 
     def read_data(self, tags, measurements):
         pass
-    
+
+    def create_retention_policy(self, name, duration, replication):
+        self.client.create_retention_policy(
+            name,
+            duration,
+            replication,
+            database=self.current_database,
+            default=True
+        )
