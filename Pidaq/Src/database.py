c337d7168a7877b89612d8a9db896eec4f4474fb
Daniel Burke <danielseanburke@gmail.com>
2020-04-25 09:59:55 -0230

    Create method to read data by tag and measurement

--- Pidaq/Src/database.py
+++ Pidaq/Src/database.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import os
 
@@ -27,8 +28,26 @@ class Influx:
         }]
         self.client.write_points(table_row, tags=tags)
 
-    def read_data(self, tags, measurements):
-        pass
+    def read_data(self, tags='*', measurements=None):
+        tags_formatted = []
+        measurements_formatted = []
+        for tag in tags:
+            tags_formatted.append(f'"{tag}"')
+        for measurement in measurements:
+            measurements_formatted.append(f'"{measurement}"')
+        tags_all = ','.join(tags_formatted)
+        measurements_all = ','.join(measurements_formatted)
+        if measurements == None:
+            data = self.client.query(
+                f'SELECT {tags_all}',
+                database=self.current_database
+            )
+        else:
+            data = self.client.query(
+                f'SELECT {tags_all} FROM {measurements_all}',
+                database=self.current_database
+            )
+        return data
 
     def create_retention_policy(self, name, duration, replication):
         self.client.create_retention_policy(
@@ -38,3 +57,6 @@ class Influx:
             database=self.current_database,
             default=True
         )
+
+    def export_csv(self):
+        
