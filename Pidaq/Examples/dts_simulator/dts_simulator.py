ee249b42532ffee3d6b0d8a297b5350a319a85f6
Daniel Burke <danielseanburke@gmail.com>
2020-05-17 20:04:13 -0230

    More simulator methods and begin sim methods

--- Pidaq/Examples/dts_simulator/dts_simulator.py
+++ Pidaq/Examples/dts_simulator/dts_simulator.py
@@ -1,9 +1,11 @@
+import time
 from random import random
 from enum import Enum
 
 import can
 
-class DTSStates(Enum):
+
+class DTSState(Enum):
     START_STATE = 0
     PRE_CHARGE_INITIAL = 1
     PRE_CHARGE_ACTIVE = 2
@@ -16,38 +18,51 @@ class DTSStates(Enum):
 
 
 class DTSSimulator:
-    
-    def __init__(self):
-        self.temperature1 = can.Message(arbitration_id=160)
-        self.temperature2 = can.Message(arbitration_id=161)
-        self.temperature3 = can.Message(arbitration_id=162)
-        self.analog_input_voltages = can.Message(arbitration_id=163)
-        self.digital_input_status = can.Message(arbitration_id=164)
-        self.motor_position_information = can.Message(arbitration_id=165)
-        self.current_information = can.Message(arbitration_id=166)
-        self.voltage_information = can.Message(arbitration_id=167)
-        self.flux_information = can.Message(arbitration_id=168)
-        self.internal_voltages = can.Message(arbitration_id=169)
-        self.internal_states = can.Message(arbitration_id=170)
-        self.fault_codes = can.Message(arbitration_id=171)
-        self.torque_timer_info = can.Message(arbitration_id=172)
-        self.modulation_index_flux_weakening = can.Message(arbitration_id=173)
-        self.firmware_information = can.Message(arbitration_id=174)
+
+    def __init__(self, bus_name: str):
+
+        # Configure bus
+        self.bus = can.interfaces.socketcan.SocketcanBus(bus_name)
+
+        # Configure messages
+        self.temperature1 = can.Message(arbitration_id=160, dlc=8)
+        self.temperature2 = can.Message(arbitration_id=161, dlc=8)
+        self.temperature3 = can.Message(arbitration_id=162, dlc=8)
+        self.analog_input_voltages = can.Message(arbitration_id=163, dlc=8)
+        self.digital_input_status = can.Message(arbitration_id=164, dlc=8)
+        self.motor_position_information = can.Message(
+            arbitration_id=165,
+            dlc=8
+        )
+        self.current_information = can.Message(arbitration_id=166, dlc=8)
+        self.voltage_information = can.Message(arbitration_id=167, dlc=8)
+        self.flux_information = can.Message(arbitration_id=168, dlc=8)
+        self.internal_voltages = can.Message(arbitration_id=169, dlc=8)
+        self.internal_states = can.Message(arbitration_id=170, dlc=8)
+        self.fault_codes = can.Message(arbitration_id=171, dlc=8)
+        self.torque_timer_info = can.Message(arbitration_id=172, dlc=8)
+        self.modulation_index_flux_weakening = can.Message(
+            arbitration_id=173,
+            dlc=8
+        )
+        self.firmware_information = can.Message(arbitration_id=174, dlc=8)
 
     def update_temperature1(self):
         module_a_temperature = int(200 + random() * 5.0).to_bytes(2, 'big')
         module_b_temperature = int(210 + random() * 4.5).to_bytes(2, 'big')
         module_c_temperature = int(205 + random() * 3.7).to_bytes(2, 'big')
-        gate_driver_board_temperature = int(190 + random() * 6.2).to_bytes(2, 'big')
+        gate_driver_board_temperature = int(
+            190 + random() * 6.2).to_bytes(2, 'big')
         self.temperature1.data = [
             module_a_temperature,
             module_b_temperature,
             module_c_temperature,
             gate_driver_board_temperature
         ]
-    
+
     def update_temperature2(self):
-        control_board_temperature = int(240 + random() * 4.5).to_bytes(2, 'big')
+        control_board_temperature = int(
+            240 + random() * 4.5).to_bytes(2, 'big')
         rtd_1_temperature = int(230 + random() * 6.3).to_bytes(2, 'big')
         rtd_2_temperature = int(225 + random() * 3.4).to_bytes(2, 'big')
         rtd_3_temperature = int(220 + random() * 2.3).to_bytes(2, 'big')
@@ -57,12 +72,12 @@ class DTSSimulator:
             rtd_2_temperature,
             rtd_3_temperature
         ]
-    
+
     def update_temperature3(self):
         rtd_4_temperature = int(203 + random() * 2.1).to_bytes(2, 'big')
         rtd_5_temperature = int(256 + random() * 3.9).to_bytes(2, 'big')
         motor_temperature = int(232 + random() * 4.2).to_bytes(2, 'big')
-        torque_shudder = None # TODO once set torque value gets stored
+        torque_shudder = None  # TODO once set torque value gets stored
         self.temperature3.data = [
             rtd_4_temperature,
             rtd_5_temperature,
@@ -83,16 +98,16 @@ class DTSSimulator:
         ]
 
     def update_digital_input_status(self):
-        pass # This will need to read from the state
+        pass  # This will need to read from the state
 
     def update_motor_position_information(self):
-        pass # This will need to read from the state
+        pass  # This will need to read from the state
 
     def update_current_information(self):
         phase_a_current = int(20 + random() * 5).to_bytes(2, 'big')
         phase_b_current = int(20 + random() * 5).to_bytes(2, 'big')
         phase_c_current = int(20 + random() * 5).to_bytes(2, 'big')
-        dc_bus_current = int(15 + random() * 6).to_bytes(2, 'big') 
+        dc_bus_current = int(15 + random() * 6).to_bytes(2, 'big')
         self.current_information = [
             phase_a_current,
             phase_b_current,
@@ -125,10 +140,10 @@ class DTSSimulator:
         ]
 
     def update_internal_voltages(self):
-        one_five_voltage_ref = 150.to_bytes(2, 'big')
-        two_five_voltage_ref = 250.to_bytes(2, 'big')
-        five_voltage_ref = 500.to_bytes(2, 'big')
-        twelve_system_voltage = 1200.to_bytes(2, 'big')
+        one_five_voltage_ref = int(150).to_bytes(2, 'big')
+        two_five_voltage_ref = int(250).to_bytes(2, 'big')
+        five_voltage_ref = int(500).to_bytes(2, 'big')
+        twelve_system_voltage = int(1200).to_bytes(2, 'big')
         self.internal_voltages = [
             one_five_voltage_ref,
             two_five_voltage_ref,
@@ -137,13 +152,13 @@ class DTSSimulator:
         ]
 
     def update_internal_states(self):
-        pass # Read from input configuration
+        pass  # Read from input configuration
 
     def update_fault_codes(self):
         pass
 
     def update_torque_timer_information(self):
-        pass # Read from command message
+        pass  # Read from command message
 
     def update_modulation_index(self):
         pass
@@ -153,10 +168,29 @@ class DTSSimulator:
         BIT_2 = 2
         BIT_3 = 4
         self.torque_command = message.data[1] + message.data[0]
-        self.speed_command = message_data[3] + message.data[2]
+        self.speed_command = message.data[3] + message.data[2]
         self.direction_command = message.data[4]
         self.inverter_enable = message.data[5] & BIT_1
         self.inverter_discharge = message.data[5] & BIT_2
-        self.speed_mode_enable = message.data[5] & BIT_3 
+        self.speed_mode_enable = message.data[5] & BIT_3
         self.commanded_torque_limit = message.data[7] + message.data[6]
 
+    def send_information_messages_10hz(self):
+        time.sleep(1 / 10)
+        self.bus.send(self.temperature1)
+        self.bus.send(self.temperature2)
+        self.bus.send(self.temperature3)
+        self.bus.send(self.internal_voltages)
+        self.bus.send(self.fault_codes)
+
+    def send_information_messages_100hz(self):
+        time.sleep(1 / 100)
+        self.bus.send(self.analog_input_voltages)
+        self.bus.send(self.digital_input_status)
+        self.bus.send(self.motor_position_information)
+        self.bus.send(self.current_information)
+        self.bus.send(self.voltage_information)
+        self.bus.send(self.flux_information)
+        self.bus.send(self.internal_states)
+        self.bus.send(self.torque_timer_info)
+        self.bus.send(self.modulation_index_flux_weakening)
--- Pidaq/Lib/dts_manager/dts_manager.py
+++ Pidaq/Lib/dts_manager/dts_manager.py
@@ -1,9 +1,27 @@
 
 
+from can_manager import can_manager
+
+
 class DTSManager:
 
-    def __init__(self):
-        pass
+    def __init__(self, bus_name: str):
+        self.bus = can_manager.CanManager(bus_name)
 
     def configure_motor(self, configuration: dict) -> None:
-        pass
+        # Extract commands from dict
+        torque_command = int(configuration['torque']).to_bytes(2, 'big')
+        speed_command = int(configuration['speed']).to_bytes(2, 'big')
+        direction_command = int(configuration['direction']).to_bytes(1, 'big')
+        inverter_enable = int(configuration['inverterEnable'])
+        inverter_discharge = int(configuration['inverterDischarge'])
+        speed_mode_enable = int(configuration['speedModeEnable'])
+        mode = ((int(speed_mode_enable) << 2) +
+                (int(inverter_discharge) << 1) + (int(inverter_enable) << 0)).to_bytes(1, 'big')
+        commanded_torque_limit = int(
+            configuration['commandedTorqueLimit']).to_bytes(2, 'big')
+
+        # Create 8 Byte message
+        command_list = bytes(torque_command, speed_command,
+                             direction_command, mode, commanded_torque_limit)
+        self.bus.send_message(192, command_list)
