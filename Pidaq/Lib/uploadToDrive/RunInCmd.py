ae959d4f11c7805dcf93b5a0895d2c3b61fb0586
youssef-m-ali <56202498+youssef-m-ali@users.noreply.github.com>
2020-05-19 04:01:50 -0230

    Update RunInCmd.py

--- Pidaq/Lib/uploadToDrive/RunInCmd.py
+++ Pidaq/Lib/uploadToDrive/RunInCmd.py
@@ -1,14 +1,15 @@
-import uploadToDrive
+import uploadToDrive 
+import os
 
-path = input("File Path:") #Takes the file's path without quotation marks
-
-while True:
-    folder =input("Folder Name (Safety, Integrity, DTS): ")
-    if folder == 'Safety' \
-       or folder == 'Integrity' \
-       or folder == 'DTS':
+while 1:
+    # Takes the file's path without quotation marks
+    CsvPath = input("File Path:")
+    # Exits the loop if the path exists
+    if os.path.exists(CsvPath):
         break
-        
+# prints a list of the folder names available in the json file
+print(uploadToDrive.parentDictionary.keys())
+folder = input("Folder Name: ")
 
 uploadToDrive.uploadCsv(path, folder)
 
