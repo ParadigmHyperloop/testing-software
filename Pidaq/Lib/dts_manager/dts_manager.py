d7a5357e6d239f22a7712b9dbde4595de2701ce9
Daniel Burke <danielseanburke@gmail.com>
2020-05-20 23:19:46 -0230

    Define update motor position method, and begin defining interface methods

--- Pidaq/Lib/dts_manager/dts_manager.py
+++ Pidaq/Lib/dts_manager/dts_manager.py
@@ -10,18 +10,23 @@ class DTSManager:
 
     def configure_motor(self, configuration: dict) -> None:
         # Extract commands from dict
-        torque_command = int(configuration['torque']).to_bytes(2, 'big')
-        speed_command = int(configuration['speed']).to_bytes(2, 'big')
-        direction_command = int(configuration['direction']).to_bytes(1, 'big')
-        inverter_enable = int(configuration['inverterEnable'])
-        inverter_discharge = int(configuration['inverterDischarge'])
-        speed_mode_enable = int(configuration['speedModeEnable'])
-        mode = ((int(speed_mode_enable) << 2) +
-                (int(inverter_discharge) << 1) + (int(inverter_enable) << 0)).to_bytes(1, 'big')
-        commanded_torque_limit = int(
+        self.torque_command = int(configuration['torque']).to_bytes(2, 'big')
+        self.speed_command = int(configuration['speed']).to_bytes(2, 'big')
+        self.direction_command = int(
+            configuration['direction']).to_bytes(1, 'big')
+        self.inverter_enable = int(configuration['inverterEnable'])
+        self.inverter_discharge = int(configuration['inverterDischarge'])
+        self.speed_mode_enable = int(configuration['speedModeEnable'])
+        self.mode = ((int(self.speed_mode_enable) << 2) +
+                     (int(self.inverter_discharge) << 1) + (int(self.inverter_enable) << 0)).to_bytes(1, 'big')
+        self.commanded_torque_limit = int(
             configuration['commandedTorqueLimit']).to_bytes(2, 'big')
 
-        # Create 8 Byte message
-        command_list = bytes(torque_command, speed_command,
-                             direction_command, mode, commanded_torque_limit)
+    def send_motor_command(self) -> None:
+        command_list = bytes(self.torque_command, self.speed_command,
+                             self.direction_command, self.mode, self.commanded_torque_limit)
         self.bus.send_message(192, command_list)
+
+    def convert_temperatures(self) -> None:
+        module_a_temperature = int.from_bytes(self.bus.messages['0x0A0'].data[1] +
+                                              self.bus.messages['0x0A0'].data[0], byteorder='big')
