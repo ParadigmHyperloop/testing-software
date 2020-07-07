1631e5e820fbbf5d8ab678ab95d7bc28cfd4ca1f
COLTON SMITH <cnsmith@mun.ca>
2020-07-07 17:01:29 -0230

    Remove unwanted imports

--- Pidaq/UI/DTS/dts-dash.py
+++ Pidaq/UI/DTS/dts-dash.py
@@ -1,9 +1,5 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_daq as daq
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
 
 from dts_dash.app import app
 from dts_dash.callbacks import control_callbacks
