dad9eb343565f36c6e6b8c33a91b21c0be3c477c
Daniel Burke <danielseanburke@gmail.com>
2020-08-26 15:40:32 -0230

    create example main which reads internal and external adc readings, and sends them over the can bus

--- Node/Examples/exampleMain.cpp
+++ Node/Examples/exampleMain.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 
+#include "adc_ADS114S0.h"
 #include "mcp2515.h"
+
+#define EADC_SS 7
+#define CAN_SS 18
+
+MCP2515 mcp2515{CAN_SS};
+ADS114S0 ads114s0{EADC_SS};
 
 void setup()
 {
     pinMode(PIN_SPI_SS, OUTPUT);
+    pinMode(A0, INPUT);
 
-    MCP2515 mcp2515{PIN_SPI_SS};
+    ads114s0.reset();
+    ads114s0.setMux(MUX_SINGLE_0);
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    uint16_t internalReading = analogRead(A0);
+    uint16_t externalReading = ads114s0.readData();
+
+    Serial.println(internalReading);
+    Serial.println(externalReading);
+
+    can_frame internalMessage = { 200, 2, { (uint8_t)((internalReading & 0xff00) >> 8), (uint8_t)(internalReading & 0x00ff) } };
+    can_frame externalMessage = { 201, 2, { (uint8_t)((externalReading & 0xff00) >> 8), (uint8_t)(externalReading & 0x00ff) } };
+
+    mcp2515.sendMessage(&internalMessage);
+    mcp2515.sendMessage(&externalMessage);
 }
