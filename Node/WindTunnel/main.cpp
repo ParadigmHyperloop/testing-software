cb89f6ab9afb4026cc25b3c90637fa30a7adaf59
Daniel Burke <danielseanburke@gmail.com>
2020-09-02 16:58:50 -0230

    Continued definition of main code using newly created SensorReading and CANMessage classes

--- Node/WindTunnel/main.cpp
+++ Node/WindTunnel/main.cpp
@@ -7,22 +7,36 @@ Dps310 pressure;
 #include "adc_ADS114S0.h"
 #include "mcp2515.h"
 
+#include "windTunnel.h"
+
 #define EADC_SS 8
 #define CAN_SS 9
 
 MCP2515 mcp2515{CAN_SS};
 ADS114S0 ads114s0{EADC_SS};
 
+SensorReading readings[] = {};
+CANMessage messages[] = {};
+
 void setup()
 {
+<<<<<<< HEAD
     pinMode(LED_BUILTIN, OUTPUT);
     pressure.begin(Wire);
+=======
+>>>>>>> 9e38a53... Continued definition of main code using newly created SensorReading and CANMessage classes
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    for (uint8_t i = 0; i < sizeof(readings) / sizeof(readings[0]); ++i)
+    {
+        readings[i].read();
+    }
+
+    for (uint8_t i = 0; i < sizeof(messages) / sizeof(messages[0]); ++i)
+    {
+        messages[i].updateMessageData();
+        messages[i].send(&mcp2515);
+    }
 }
