7117a76e27958efe56af9581a990eb3990bd0070
kmyali <kmali@mun.ca>
2020-09-18 00:22:17 -0230

    Add path functionality and comments

--- Pidaq/Lib/wind_tunnel/example.py
+++ Pidaq/Lib/wind_tunnel/example.py
@@ -1,7 +1,24 @@
 from air_properties import linearInterpolation
+import os
+import logging
 
-interObject = linearInterpolation() # Create the object
-x = interObject.interpolateDensity(-76) # interpolate density
-y = interObject.interpolateViscosity(-76) # interpolate dynamic viscosity 
-print(x) 
-print(y)
+logger = logging.getLogger()
+
+while 1:
+    filePath = input("CSV File Path, no quotations: ")
+    
+    # Exits the loop if the path is valid
+    if os.path.exists(filePath):
+        break
+    else:
+        logger.warning("Path does not exist. \nPlease enter a valid path: ")
+
+interObject = linearInterpolation(filePath)
+
+value = input("Enter temperature value, or write \"exit\" to end: ")
+while(value != "exit"):
+    x = interObject.interpolateDensity(value)
+    y = interObject.interpolateViscosity(value) 
+    print(x) 
+    print(y)
+    value = input("Enter next temperature value, or write \"exit\" to end: ")
