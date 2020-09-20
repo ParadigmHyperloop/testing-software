6aefcf0cb29e914b4ec612c8567c14d336e97252
afhassan <afayek@mun.ca>
2020-09-20 15:29:52 -0230

    Fixed some layout issues

--- Pidaq/UI/Stability/stability_dash/layout/control.py
+++ Pidaq/UI/Stability/stability_dash/layout/control.py
@@ -102,14 +102,14 @@ dbc.Row([
     dbc.Col([
         dbc.Card([
             dbc.ButtonGroup([
-                dbc.Button("SAVE PROFILE", id="save-profile"),
-                dbc.Button("DELETE PROFILE", id="delete-profile"),
+                dbc.Button("SAVE PROFILE", id="save-profile", style={"height":"100%","width":"100%"}),
+                dbc.Button("DELETE PROFILE", id="delete-profile", style={"height":"100%","width":"100%"}),
                 dcc.Dropdown(
                     id = "load-profile",
                     options = [
                     ],
                     placeholder="LOAD PROFILE",
-                    style={'width': '70%'})
+                    style={'width': '100%'})
             ])
         ],
         body=True)  
@@ -120,28 +120,28 @@ dbc.Row([
 dbc.Row([
     dbc.Col([
         dbc.Card([
-            dbc.Button("RECORD", id="record", color="success", block = True)
-        ])  
+            dbc.Button("RECORD", id="record", color="success", style={"height":"100%","width":"100%"})
+        ], style={"height":"100%","width":"100%"})  
     ],
     width={"size": 1, "offset": 4}),
     dbc.Col([
         dbc.Card([
-            dbc.Button("TARE", color="warning", block = True),
-            dbc.Button("CLEAR DATA", color="warning", block = True)
+            dbc.Button("TARE", color="warning"),
+            dbc.Button("CLEAR DATA", color="warning")
         ])  
     ],
     width={"size": 2, "offset": 0}),
     dbc.Col([
         dbc.Card([
-            dbc.Button("STOP", color="danger", block = True)
-        ])  
+            dbc.Button("STOP", id="stop", color="danger", style={"height":"100%","width":"100%"})
+        ], style={"height":"100%","width":"100%"})  
     ],
     width={"size": 1, "offset": 0})
 ]),
 
 dbc.Row([
     dbc.Col([
-        dbc.Card([dbc.Button("EMERGENCY STOP", color="danger", block = True )] , body=True)  
+        dbc.Card([dbc.Button("EMERGENCY STOP", color="danger", size="lg", block = True, style={"height":"100%","width":"100%"} )])  
     ],
     width={"size": 4, "offset": 4})
 ]),
