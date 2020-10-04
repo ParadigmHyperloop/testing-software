46f0c4e6e1aeaa31ad6af707e8110b2fb852ff90
Daniel Burke <danielseanburke@gmail.com>
2020-10-04 13:37:44 -0230

    Remove I2C device from SensorReading
    The DPS310 gives it's readings as 24-bit 2s complement floats,
    which did not fit with the SensorReading's unsigned data representation.
    Created PressureSensor class to wrap Dps310 class

--- Node/WindTunnel/windTunnel.cpp
+++ Node/WindTunnel/windTunnel.cpp
@@ -9,32 +9,28 @@
 #define EADC_SS 8
 #define CAN_SS 9
 
-MCP2515 mcp2515{CAN_SS};
-ADS114S0 ads114s0{EADC_SS};
+MCP2515 CanBus{CAN_SS};
+ADS114S0 EADC{EADC_SS};
 
-SensorReading readings[] = {};
-CANMessage messages[] = {};
+void PressureSensor::measurePressure()
+{
+    m_dps310.measurePressureOnce(m_result);
+}
 
 void SensorReading::read()
 {
-    switch (type)
+    switch (m_type)
     {
         case Internal:
         {
-            data = analogRead(address);
+            m_data = analogRead(m_address);
             break;
         }
 
         case External:
         {
-            ads114s0->setMux(static_cast<InputMux>(address));
-            data = ads114s0->readData();
-            break;
-        }
-
-        case I2C:
-        {
-            // TODO
+            m_ads114s0->setMux(static_cast<InputMux>(m_address));
+            m_data = m_ads114s0->readData();
             break;
         }
     }
@@ -42,35 +38,24 @@ void SensorReading::read()
 
 void CANMessage::updateMessageData()
 {
-    uint8_t i = 0;
-    uint8_t j = 0;
-    for (; i < 4; ++i)
+    if (m_srReadings)
     {
-        frame.data[j++] = (readings[i]->data & 0xff00) >> 8;
-        frame.data[j++] = readings[i]->data & 0x00ff;
+        uint8_t i = 0;
+        uint8_t j = 0;
+        for (; i < 4; ++i)
+        {
+            frame.data[j++] = (m_srReadings[i]->getData() & 0xff00) >> 8;
+            frame.data[j++] = m_srReadings[i]->getData() & 0x00ff;
+        }
+    }
+    else // Pressure Reading
+    {
+        memcpy(frame.data, &(m_pReadings[1]->getResult()), sizeof(float));
+        memcpy(frame.data + 4, &(m_pReadings[1]->getResult()), sizeof(float));
     }
 }
 
-void CANMessage::send(MCP2515* mcp2515)
+void CANMessage::send(MCP2515* bus)
 {
-    mcp2515->sendMessage(&frame);
-}
-
-void setup()
-{
-    Wire.begin();
-}
-
-void loop()
-{
-    for (uint8_t i = 0; i < sizeof(readings) / sizeof(readings[0]); ++i)
-    {
-        readings[i].read();
-    }
-
-    for (uint8_t i = 0; i < sizeof(messages) / sizeof(messages[0]); ++i)
-    {
-        messages[i].updateMessageData();
-        messages[i].send(&mcp2515);
-    }
+    bus->sendMessage(&frame);
 }
--- Node/WindTunnel/windTunnel.h
+++ Node/WindTunnel/windTunnel.h
@@ -1,31 +1,54 @@
+#ifndef WINDTUNNEL_H
+#define WINDTUNNEL_H
+
 #include <Wire.h>
 
 #include "adc_ADS114S0.h"
 #include "can.h"
+#include "Dps310.h"
 #include "mcp2515.h"
 
 enum ReadingType
 {
     Internal,
-    External,
-    I2C
+    External
 };
 
-struct SensorReading
+class PressureSensor
 {
+public:
+    void measurePressure();
+    float& getResult(){ return m_result; } 
+
+private:
+    Dps310 m_dps310;
+    float m_result;
+};
+
+class SensorReading
+{
+public:
     void read();
+    void begin();
+    uint16_t getData(){ return m_data; }
 
-    ADS114S0* ads114s0; // nullptr if reading is I2C or Internal
-    ReadingType type;
-    uint8_t address; // Can either be a pin # or an I2C address
-    uint16_t data;
+private:
+    ADS114S0* m_ads114s0; // nullptr if reading is I2C or Internal
+    ReadingType m_type;
+    uint8_t m_address; // Can either be a pin # or an I2C address
+    uint16_t m_data;
 };
 
-struct CANMessage
+class CANMessage
 {
+public:
     void updateMessageData();
     void send(MCP2515* mcp2515);
 
-    SensorReading* readings[4];
+private:
+    SensorReading* m_srReadings[4];
+    PressureSensor* m_pReadings[2];
     can_frame frame;
 };
+
+#endif // WINDTUNNEL_H
