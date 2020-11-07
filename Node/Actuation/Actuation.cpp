1ec62ce1bd32bcf5785048d84784d6e68b7d4c2a
Daniel Burke <danielseanburke@gmail.com>
2020-11-07 16:44:44 -0330

    Continue defining command response methods

--- Node/Actuation/Actuation.cpp
+++ Node/Actuation/Actuation.cpp
@@ -19,11 +19,14 @@ bool ActuationManager::receiveCommand()
         {
             uint16_t u16Data = (frame.data[0] << 8) | frame.data[1];
             uint8_t u8Address = frame.data[2];
-            InitializeStepper(u16Data, u8Address);
+            InitStepper(u16Data, u8Address);
             break;
         }
         case (eSpeedCommand):
         {
+            uint16_t u16Data = (frame.data[0] << 8) | frame.data[1];
+            uint8_t u8Address = frame.data[2];
+            setStepperSpeed(u16Data, u8Address);
             break;
         }
         case (eStepCommand):
@@ -35,9 +38,54 @@ bool ActuationManager::receiveCommand()
     return true;
 }
 
-void AcutationManager::InitializeStepper(uint16_t u16Step, uint8_t u8Address)
+void ActuationManager::InitStepper(uint16_t u16Step, uint8_t u8Address)
 {
+    if (u8Address != 1 || u8Address != 2)
+    {
+        // TODO error handling
+        return;
+    }
+    if (m_apSteppers[u8Address - 1] == nullptr)
+    {
+        m_apSteppers[u8Address - 1] = m_motorShield.getStepper(u16Step, u8Address);
+    }
+    sendInitStepperResponse(u16Step, u8Address);
+}
 
+void ActuationManager::sendInitStepperResponse(uint16_t u16Step, uint8_t u8Address)
+{
+    uint8_t au8Data[] = { (uint8_t)((u16Step & 0xFF00) >> 8), (uint8_t)(u16Step & 0x00FF), u8Address };
+    can_frame frame = { eInitResponse, 8, au8Data };
+
+    MCP2515::ERROR status = m_can.sendMessage(&frame);
+    if (status != MCP2515::ERROR_OK)
+    {
+        // TODO error handling
+    }
+}
+
+void ActuationManager::setStepperSpeed(uint16_t u16Speed, uint8_t u8Address)
+{
+    if (m_apSteppers[u8Address - 1] == nullptr)
+    {
+        // TODO error handling
+        return;
+    }
+    m_apSteppers[u8Address - 1]->setSpeed(u16Speed);
+
+    sendStepperSpeedResponse(u16Speed, u8Address);
+}
+
+void ActuationManager::sendStepperSpeedResponse(uint16_t u16Speed, uint8_t u8Address)
+{
+    uint8_t au8Data[] = { (uint8_t)((u16Speed & 0xFF00) >> 8), (uint8_t)(u16Speed & 0x00FF), u8Address };
+    can_frame frame = { eSpeedResponse, 8, au8Data };
+
+    MCP2515::ERROR status = m_can.sendMessage(&frame);
+    if (status != MCP2515::ERROR_OK)
+    {
+        // TODO error handling
+    }
 }
 
 int main(void)
--- Node/Actuation/Actuation.h
+++ Node/Actuation/Actuation.h
@@ -1,5 +1,3 @@
-#include <array>
-
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
@@ -25,11 +23,15 @@ private:
         eStepResponse = 22
     };
 
-    void InitializeStepper(uint16_t, uint8_t);
+    void InitStepper(uint16_t, uint8_t);
+    void sendInitStepperResponse(uint16_t, uint8_t);
+
+    void setStepperSpeed(uint16_t, uint8_t);
+    void sendStepperSpeedResponse(uint16_t, uint8_t);
 
     TwoWire* m_pI2C;
     MCP2515 m_can;
     Adafruit_MotorShield m_motorShield;
-    std::array<Adafruit_StepperMotor*, 2> m_apSteppers;
+    Adafruit_StepperMotor* m_apSteppers[2];
 };
 
